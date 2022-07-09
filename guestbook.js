abb60b1573986e1d7a2e87d7096fdd2d3f32789e
Abdallah Mtavya <67534237+Mtavya420@users.noreply.github.com>
2022-07-09 20:00:33 +0900

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://9ae55b3b.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
